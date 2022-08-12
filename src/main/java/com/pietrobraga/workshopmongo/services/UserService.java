9de9efaf8bc4c6707ca341ffa83a5618637e5af8
Piêtro Braga <pietrobragaprofissional@gmail.com>
2022-08-11 23:05:03 -0300

    PUT atualizar usuario

--- src/main/java/com/pietrobraga/workshopmongo/services/UserService.java
+++ src/main/java/com/pietrobraga/workshopmongo/services/UserService.java
@@ -38,4 +38,17 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(User user) {
+		User newUser = repository.findById(user.getId()).get();
+		updateData(newUser, user);
+		return repository.save(newUser);
+	}
+
+	private void updateData(User newUser, User user) {
+		
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+		
+	}
+	
 }
