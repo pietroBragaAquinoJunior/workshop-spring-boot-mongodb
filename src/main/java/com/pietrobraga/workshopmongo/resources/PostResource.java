4021516b42a2d116daff784ad41890cee5cd4fba
Piêtro Braga <pietrobragaprofissional@gmail.com>
2022-08-12 01:48:52 -0300

    obtendo post por id

---
+++ src/main/java/com/pietrobraga/workshopmongo/resources/PostResource.java
@@ -0,0 +1,28 @@
+package com.pietrobraga.workshopmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pietrobraga.workshopmongo.domain.Post;
+import com.pietrobraga.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+	
+	@Autowired
+	PostService postService;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id){
+		
+		Post post = postService.findById(id);
+		return ResponseEntity.ok().body(post);
+		
+	}
+	
+}
---
+++ src/main/java/com/pietrobraga/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.pietrobraga.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pietrobraga.workshopmongo.domain.Post;
+import com.pietrobraga.workshopmongo.repositories.PostRepository;
+import com.pietrobraga.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	PostRepository repository;
+
+
+	public Post findById(String id) {
+		Post post = repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
+		return post;
+	}
+	
+	
+	
+}
