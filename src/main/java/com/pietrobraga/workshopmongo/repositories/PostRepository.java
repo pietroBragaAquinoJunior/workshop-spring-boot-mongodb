a69855e8b6744004cb70cf6c6a353013e4f640aa
Piêtro Braga <pietrobragaprofissional@gmail.com>
2022-08-12 22:07:14 -0300

    consulta usando @query

--- src/main/java/com/pietrobraga/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/pietrobraga/workshopmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.pietrobraga.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.pietrobraga.workshopmongo.domain.Post;
@@ -14,5 +15,8 @@ import com.pietrobraga.workshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 
 	   List<Post> findByTitleContainingIgnoreCase(String text);
-	
+	   
+	   @Query("{'title' : { $regex: ?0, $options: 'i'}}")
+	   List<Post> searchTitle(String text);
+	   
 }
